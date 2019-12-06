65412a6386abde7178a97d83350d721de6892833
Miguel Ben <itsmigben@gmail.com>
2019-12-06 22:16:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,12 +8,25 @@ def load_library(file)
     'get_meaning' => {},
     "get_emoji" => {}
   }
+  
+  emojis.each do |k, v|
+    hash['get_emoji'][v[0]] = emojis[k][1]
+    hash['get_meaning'][v[1]] = k
+  end
+  
+  hash
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file, emoji)
   # code goes here
+  result = load_library(file)['get_emoji'][emoji]
+  # im confuse at this point should I call this emoji or emoticon - need to google this ! the real question here.
+  result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoji)
   # code goes here
+  result = load_library(file)['get_meaning'][emoji]
+  result ? result : "Sorry, that emoticon was not found"
 end
